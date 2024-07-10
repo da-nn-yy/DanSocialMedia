f80b7fc2c493f50c5511a58f699a1da063ab8801
da-nn-yy <db7394928@gmail.com>
2024-07-11 01:32:46 +0200

    adding style to navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
 import {auth} from "../config/frbsConfig"
 import {useAuthState} from 'react-firebase-hooks/auth'
+import './Navbar.css'
 
 export const Navbar = () => {
 
   const [user]  = useAuthState(auth)
   return (
-    <div>
-      <Link to="/"> Home </Link>
-      <Link to="/login">Login</Link>
-
-      <div>
-        <p>{user?.displayName}</p>
-        <img src={user?.photoURL || ""} alt="" width="50px" />
+    <div className="nav">
+        <h1>DAN</h1>
+        
+        <div className="log">
+          <div className="link">
+            <Link to="/"> Home </Link>
+            <Link to="/login">Login</Link>
+          </div>
+                
+        <div className="ggl">
+          <p>{user?.displayName}</p>
+          <img src={user?.photoURL || ""} alt="" width="50px" />
+        </div>
       </div>
     </div>
   )
