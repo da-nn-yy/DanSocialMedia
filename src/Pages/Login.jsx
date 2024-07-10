0980f889870f7949a2dbae4f57e4c74d2ea43d0a
da-nn-yy <db7394928@gmail.com>
2024-07-11 00:39:57 +0200

    modified :  my login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,15 @@
+import {auth,provider} from "../config/frbsConfig";
+import { signInWithPopup } from "firebase/auth";
+
 export const Login = () => {
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+  }
   return (
-    <div>Login page</div>
+    <div>
+      <p>Sign in with Google to continue</p>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
   )
 }
