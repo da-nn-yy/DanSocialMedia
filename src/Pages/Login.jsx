2f4735556c6158c937265e3ea63194d4752e90b3
da-nn-yy <db7394928@gmail.com>
2024-07-11 01:17:37 +0200

    installed rct-firebase hooks package

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
 
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
 
   const signInWithGoogle = async () => {
     const result = await signInWithPopup(auth, provider)
