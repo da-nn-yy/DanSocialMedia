10277eb422f414db9e66f0cb73de0ccc110debcb
da-nn-yy <db7394928@gmail.com>
2024-07-11 01:21:56 +0200

    modifed: navar.jsx with firbase-hook

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import {auth} from "../config/frbsConfig"
+import {useAuthState} from 'react-firebase-hooks/auth'
 
 export const Navbar = () => {
+
+  const [user]  = useAuthState(auth)
   return (
     <div>
       <Link to="/"> Home </Link>
       <Link to="/login">Login</Link>
 
       <div>
-        <p>{auth.currentUser.displayName}</p>
-        <img src={auth.currentUser?.photoURL || ""} alt="" width="50px" />
+        <p>{user?.displayName}</p>
+        <img src={user?.photoURL || ""} alt="" width="50px" />
       </div>
     </div>
   )
