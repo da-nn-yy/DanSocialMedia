704ab19c907124af155dcc1205b8d2ff8d042cef
da-nn-yy <db7394928@gmail.com>
2024-07-11 01:13:08 +0200

    modified : navbar.jsx with image of user and name

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import {auth} from "../config/frbsConfig"
 
 export const Navbar = () => {
   return (
     <div>
       <Link to="/"> Home </Link>
       <Link to="/login">Login</Link>
+
+      <div>
+        <p>{auth.currentUser.displayName}</p>
+        <img src={auth.currentUser?.photoURL || ""} alt="" width="50px" />
+      </div>
     </div>
   )
 }
