2fac43eb9ee65723089692d6854fc2e478b282d6
da-nn-yy <db7394928@gmail.com>
2024-07-11 02:08:13 +0200

    stylingL nav bar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,25 +2,33 @@ import { Link } from "react-router-dom";
 import {auth} from "../config/frbsConfig"
 import {useAuthState} from 'react-firebase-hooks/auth'
 import './Navbar.css'
+import { signOut } from "firebase/auth";
 
 export const Navbar = () => {
 
   const [user]  = useAuthState(auth)
+
+  const signUserOut = async () => {
+    await signOut(auth) 
+  }
   return (
-    <div className="nav">
-        <h1>DAN</h1>
-        
-        <div className="log">
-          <div className="link">
-            <Link to="/"> Home </Link>
-            <Link to="/login">Login</Link>
+      <div className="navbar">
+          <h1>DAN</h1>
+
+            <div className="links">
+              <Link className="link" to="/"> Home </Link>
+              <Link className="link" to="/login">Login</Link>
+            </div>
+      
+          <div className="user">
+            {user && (
+             <>
+              <p>{user?.displayName}</p>
+              <img src={user?.photoURL || ""} alt=""/>
+              <button onClick={signUserOut}>Log Out</button>
+             </>
+            )}
           </div>
-                
-        <div className="ggl">
-          <p>{user?.displayName}</p>
-          <img src={user?.photoURL || ""} alt="" width="50px" />
         </div>
-      </div>
-    </div>
   )
 }
