a7d39bdddc11cf076191b2959b90193841f23f0f
da-nn-yy <db7394928@gmail.com>
2024-07-11 02:20:52 +0200

    styled : navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -13,21 +13,23 @@ export const Navbar = () => {
   }
   return (
       <div className="navbar">
-          <h1>DAN</h1>
+           <h1>DAN</h1>
 
-            <div className="links">
-              <Link className="link" to="/"> Home </Link>
-              <Link className="link" to="/login">Login</Link>
-            </div>
-      
-          <div className="user">
-            {user && (
-             <>
-              <p>{user?.displayName}</p>
-              <img src={user?.photoURL || ""} alt=""/>
-              <button onClick={signUserOut}>Log Out</button>
-             </>
-            )}
+            <div className="btween">
+              <div className="links">
+                <Link className="link" to="/"> Home </Link>
+                <Link className="link" to="/login">Login</Link>
+              </div>
+                    
+            <div className="user">
+              {user && (
+               <>
+                <p>{user?.displayName}</p>
+                <img src={user?.photoURL || ""} alt=""/>
+                <button onClick={signUserOut}>Log Out</button>
+               </>
+              )}
+              </div>
           </div>
         </div>
   )
