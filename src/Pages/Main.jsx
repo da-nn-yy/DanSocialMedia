f987424eee021ff8712960f404cceb35f3d26fee
da-nn-yy <db7394928@gmail.com>
2024-07-14 12:55:38 +0200

    modified main or home page

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,6 +1,11 @@
-
+import { getDoc,collection   } from "firebase/firestore"
+import { db } from "../config/frbsConfig"
+import { useState } from "react"
 
 export const Main = () => {
+  const [postList,setPostList] = useState(null)
+
+  const postRef = collection(db,"posts")
   return (
     <div>Home page</div>
   )
