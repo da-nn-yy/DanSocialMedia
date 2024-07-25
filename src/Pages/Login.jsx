0eff1c73301172531519cfd0435960a3c447719c
da-nn-yy <db7394928@gmail.com>
2024-07-25 22:25:22 +0200

    styled login page and modified post

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,7 @@ import {auth,provider} from "../config/frbsConfig";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
+
 export const Login = () => {
 
   const navigate = useNavigate();
@@ -12,9 +13,9 @@ export const Login = () => {
     navigate("/");
   }
   return (
-    <div>
-      <p>Sign in with Google to continue</p>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",paddingTop:"4rem"}}>
+      <p style={{paddingBottom:"1rem",fontWeight:"bold",fontSize:"1.3rem"}}>Sign in with Google to continue</p>
+      <button className="btn" onClick={signInWithGoogle} style={{padding:"0.9rem",border:"none",borderRadius:"10px",backgroundColor:"#170877",color:"white", width:"10rem"}}>Sign in with Google</button>
     </div>
   )
 }
--- src/Pages/main/postTs.tsx
+++ src/Pages/main/postTs.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import { Post as IPost } from "./MainTs"
+import { addDoc, collection } from "firebase/firestore"
+import { auth, db } from "../../config/frbsConfig"
+import { useAuthState } from "react-firebase-hooks/auth"
 
 interface Props {
   post:IPost
@@ -7,16 +10,27 @@ interface Props {
 }
 export const PostTs =(props: Props) => {
   const {post} = props;
+
+  const [user]  = useAuthState(auth);
+  const likesRef = collection(db,"likes   ")
+
+  const addLike = async () => {
+    await addDoc(likesRef,{
+      userId: user?.uid,
+      postId: post.id
+    });
+  }
   return(
     <div>
       <div className="title">
-        <h2 style={{fontSize:"3rem"}}>{post.title}</h2>
+        <h2>{post.title}</h2>
       </div>
       <div className="descrp">
         <p>{post.description}</p>
       </div>
       <div className="footer">
         <p>@{post.username}</p>
+        <button> &#128077;</button>
       </div>
     </div>
   )
